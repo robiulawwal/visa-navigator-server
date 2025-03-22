5217b07a83ad0df4b2eda13eda44952ee52480a5
robiulawwal <robiulawwal086@gmail.com>
2025-03-22 14:23:06 +0600

    added the get patch method

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function run() {
 
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
-        console.log("Pinge your deployment. You successfully connected to MongoDB!");
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // await client.close();
     }
